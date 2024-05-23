6873708eeffea003f965407c37c693122389170e
Royal-design <royaldesign1999@gmail.com>
2024-05-23 10:11:07 +0100

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,89 @@
 import { ErrorPage } from "./ErrorPage";
 import { Navbar } from "./components/Navbar/Navbar";
 import { Route, Routes } from "react-router-dom";
-import { Shop } from "./pages/Shop";
-import { ShopCategory } from "./pages/ShopCategory";
-import { Product } from "./pages/Product";
-import { Cart } from "./pages/Cart";
+
 import { Footer } from "./components/Footer/Footer";
 import manImage from "./assets/pc4.png";
 import womanImage from "./assets/pc6.png";
 import kidimage from "./assets/pc2.png";
+import { lazy } from "react";
+import { Suspense } from "react";
+
+const Shop = lazy(() =>
+  import("./pages/Shop").then(({ Shop }) => ({ default: Shop }))
+);
+const ShopCategory = lazy(() =>
+  import("./pages/ShopCategory").then(({ ShopCategory }) => ({
+    default: ShopCategory
+  }))
+);
+const Product = lazy(() =>
+  import("./pages/Product").then(({ Product }) => ({
+    default: Product
+  }))
+);
+
+const Cart = lazy(() =>
+  import("./pages/Cart").then(({ Cart }) => ({
+    default: Cart
+  }))
+);
 
 function App() {
   return (
     <div className="">
       <Navbar />
       <Routes>
-        <Route index element={<Shop />} />
+        <Route
+          index
+          element={
+            <Suspense fallback={<p>Loading...</p>}>
+              <Shop />
+            </Suspense>
+          }
+        />
         <Route
           path="/men"
-          element={<ShopCategory category="men" image={manImage} />}
+          element={
+            <Suspense fallback={<p>Loading...</p>}>
+              <ShopCategory category="men" image={manImage} />
+            </Suspense>
+          }
         />
         <Route
           path="/women"
-          element={<ShopCategory category="women" image={womanImage} />}
+          element={
+            <Suspense fallback={<p>Loading...</p>}>
+              <ShopCategory category="women" image={womanImage} />
+            </Suspense>
+          }
         />
         <Route
           path="/kids"
-          element={<ShopCategory category="kids" image={kidimage} />}
+          element={
+            <Suspense fallback={<p>Loading...</p>}>
+              <ShopCategory category="kids" image={kidimage} />
+            </Suspense>
+          }
         />
-        <Route path="/product" element={<Product />}>
+        <Route
+          path="/product"
+          element={
+            <Suspense fallback={<p>Loading...</p>}>
+              <Product />
+            </Suspense>
+          }
+        >
           <Route path=":productId" element={<Product />} />
         </Route>
-        <Route path="/cart" element={<Cart />} />
+        <Route
+          path="/cart"
+          element={
+            <Suspense fallback={<p>Loading...</p>}>
+              <Cart />
+            </Suspense>
+          }
+        />
         <Route path="/login" element={<Shop />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
